89c3c2318b4e264665ac654cc0c4b6b8ff2ba44a
Jaloliddin <jjurakulov412@gmail.com>
2025-04-12 18:48:47 +0300

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,78 @@
+// 1-masala
+// function countBy(x, n) {
+//   return Array.from({ length: n }, (_, i) => x * (i + 1));
+// }
+
+// console.log(countBy(1, 10));
+// console.log(countBy(2, 5));
+// console.log(countBy(3, 5));
+
+// 2-masala
+// function myFunc(arr) {
+//   return arr.filter((num) => num % 2 === 0);
+// }
+
+// console.log(myFunc([1, 2, 3, 4, 5, 6, 7, 8]));
+
+// 3-masala
+// function getSum(arr) {
+//   return arr.flat().reduce((sum, num) => sum + num, 0);
+// }
+
+// const matrix = [
+//   [1, 0, 0, 0],
+//   [0, 1, 0, 0],
+//   [0, 0, 1, 0],
+//   [0, 0, 0, 5],
+// ];
+
+// console.log(getSum(matrix));
+
+// 4-masala
+// function getMax(arr) {
+//   return Math.max(...arr);
+// }
+
+// console.log(getMax([22, 55, 33, 130, 11, 66, 123]));
+
+// 5-masala
+// function getMax(arr) {
+//   return Math.max(...arr);
+// }
+
+// console.log(getMax([22, 55, 33, 130, 11, 66, 123]));
+
+// 6-masala
+// function toUpperCaseArray(arr) {
+//   return arr.map((word) => word.toUpperCase());
+// }
+
+// console.log(toUpperCaseArray(["salom", "nima", "gap"]));
+
+// 7-masala
+// function totalLetters(arr) {
+//   return arr.reduce((sum, word) => sum + word.length, 0);
+// }
+
+// console.log(totalLetters(["nimdur", "gap"]));
+
+// 8-masala
+// function sortByLength(arr) {
+//   return arr.sort((a, b) => a.length - b.length);
+// }
+
+// console.log(sortByLength(["cat", "apple", "banana", "car"]));
+
+// 9-masala
+// function filterByLength(arr) {
+//   return arr.filter((word) => word.length > 4);
+// }
+
+// console.log(filterByLength(["pear", "grape", "melon", "kiwi", "banana"]));
+
+// 10-masala
+// function getFirstLetters(arr) {
+//   return arr.map((word) => word[0]).join("");
+// }
+
+// console.log(getFirstLetters(["animal", "needle", "run", "swim"]));
